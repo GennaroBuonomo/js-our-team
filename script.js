eb89896b7b439b963627a30119ddb0e86fe6ac09
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-17 15:46:07 +0200

    Fix script js

--- script.js
+++ script.js
@@ -36,3 +36,27 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+let cards = '';
+
+for(let i=0; i<teamMembers.length; i++){
+
+  const member = teamMembers[i];
+
+  const {name, role, email, img} = member;
+
+  cards +=`<div class="col-12 col-md-4">
+        <div class="team-card">
+          <img src="${img}" alt="Marco Bianchi">
+          <div class="team-card-body">
+            <h5>${name}</h5>
+            <p>${role}</p>
+            <a href="#">${email}</a>
+          </div>
+        </div>
+      </div>`
+
+}
+
+teamContainer.innerHTML = cards;
+  
